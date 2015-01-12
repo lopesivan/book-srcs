e94afd3b4efec706f4507beb7895ecff3bbca50b
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-01-11 22:05:08 -0200

    cksum tests

--- cksum/test.c
+++ cksum/test.c
@@ -5,11 +5,10 @@ extern unsigned short cksumrfc(void *, size_t);
 extern unsigned short cksum2(void *, size_t);
 extern unsigned long TSC_READ(void);
 
-#define BUFFER_SIZE 32768+7 
+#define BUFFER_SIZE 32768+7
 char buffer[BUFFER_SIZE];
 
 void fillbuffer(void *, size_t);
-void viewbuffer(void *, size_t);
 
 int main(int argc, char *argv)
 {
@@ -27,7 +26,7 @@ int main(int argc, char *argv)
   c2 = TSC_READ() - c2;  
 
   printf("Buffer: %d bytes.\n"
-         "\tcksumrfc: 0x%04X.\t"
+         "\tcksumrfc: 0x%04X."
          "\tcksum2: 0x%04X.\n",
          BUFFER_SIZE,
          sum1, sum2); 
@@ -36,8 +35,6 @@ int main(int argc, char *argv)
          "\tchksumrfc: %lu ciclos."
          "\tchksum2: %lu ciclos.\n",
          c1, c2);
-
-  //viewbuffer(buffer, BUFFER_SIZE);
 
   return 0;
 }
@@ -51,17 +48,3 @@ void fillbuffer(void *ptr, size_t count)
     *p++ = (unsigned int)rand() >> 24;
 }
 
-void viewbuffer(void *ptr, size_t count)
-{
-  unsigned char *p = ptr;
-  int nl = 0, c = 0;
-
-  printf("\nBuffer:\n");
-  while (count--)
-  {
-    if (nl) { printf("\n"); nl = 0; }
-    if (++c == 16) { nl = 1; c = 0; }
-    printf("%02X ", *p++);
-  }
-  printf("\n");
-}
