f257d806939de0085b70033f7fe230eb89e2e66a
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-01-16 08:50:49 -0200

    Ajuste dos ciclos 'perdidos'.

--- cycle-counting/tsc-precise.asm
+++ cycle-counting/tsc-precise.asm
@@ -15,53 +15,68 @@
 
 bits 64
 
+; Este valor é calculado com base na latência das instruções, de acordo com
+; o manual de otimização da Intel. Ele também foi verificado através da
+; rotina de teste no diretório ./tests/ usando:
+;
+;   $ for i in {1..100}; do ./test; done
+;
+; O menor valor lido foi de 48, o maior foi de 112. O valor 52 aparece em
+; minhas medidas algumas vezes e é condizente com o calculo do gasto dos 13.5 ciclos de máquina.
+;
+WASTED_CYCLES equ 52
+
+section .data
+; Essas variáveis temporárias não são exportadas!
+;
+; Declarações equivalentes:
+;   static unsigned long count, tmp;
+;
+        align 8
+count:  dq    0
+tmp:    dq    0
+
 section .text
 
 global TSC_READ_START:function
 global TSC_READ_END:function
 
-; Declarações equivalentes:
-;   static unsigned long count, tmp;
-;
-; No entanto, coloquei na sessão .text para aproveitar
-; o modo de endereçamento relativo via RIP.
-;
-        align 8
-count:  dq  0
-tmp:    dq  0
-
+  align 8
 ; Protótipo:
 ;   void TSC_READ_START(void);
 ;
-  align 8
 TSC_READ_START:
   push  rbx
   cpuid
-  prefetchw [rel count]   ; Tenta garantir que as duas variáveis temporárias
-                          ; estejam no cache e prontas para serem escritas.
+  prefetchw [count]   ; Tenta garantir que as duas variáveis temporárias
+                      ; estejam no cache e prontas para serem escritas.
   rdtsc
-  mov   [rel count],eax   ; 1.5 ciclos
-  mov   [rel count+4],edx ; 1.5 ciclos
-  pop   rbx               ; 1.5 ciclos
-  ret                     ; 8 ciclos.
+  mov   [count],eax   ; 1.5 ciclos
+  mov   [count+4],edx ; 1.5 ciclos
+  pop   rbx           ; 1.5 ciclos
+  ret                 ; 8 ciclos.
 
 ; Protótipo:
-;   long TSC_READ_END(void);
+;   unsigned long TSC_READ_END(void);
 ;
   align 8
 TSC_READ_END:
                       ; call para essa função toma 1 ciclo.
   rdtscp
-  mov   [rel tmp],eax
-  mov   [rel tmp+4],edx
+  mov   [tmp],eax
+  mov   [tmp+4],edx
   push  rbx
   cpuid
-  mov   rax,[rel count]
-  sub   rax,[rel tmp]
-  sub   rax,14        ; subtrai todos os "13.5" ciclos "extras".
-  cmp   rax,0         ; if (rax <= 0) rax = 1; /* Só para garantir! */
+  mov   rax,[count]
+  sub   rax,[tmp]
+  sub   rax,WASTED_CYCLES  ; subtrai todos os "13.5" ciclos "extras".
+
+  ; if (rax <= 0)
+  ;   rax = 1;
+  cmp   rax,0
   jg    .L1
   mov   rax,1
 .L1:
+
   pop   rbx
   ret
