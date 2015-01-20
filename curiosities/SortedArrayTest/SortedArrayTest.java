b0dc292154926a5671eaf968e2295cb9fc84afb5
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-01-20 02:14:13 -0200

    Exemplo de como Java é uma porcaria

---
+++ curiosities/SortedArrayTest/SortedArrayTest.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortedArrayTest
+{
+    public static void main(String[] args)
+    {
+        // Generate data
+        int arraySize = 32768;
+        int data[] = new int[arraySize];
+
+        Random rnd = new Random(0);
+        for (int c = 0; c < arraySize; ++c)
+            data[c] = rnd.nextInt() % 256;
+
+        // !!! With this, the next loop runs faster
+        //Arrays.sort(data);
+
+        // Test
+        long start = System.nanoTime();
+        long sum = 0;
+
+        for (int i = 0; i < 100000; ++i)
+        {
+            // Primary loop
+            for (int c = 0; c < arraySize; ++c)
+            {
+                if (data[c] >= 128)
+                    sum += data[c];
+            }
+        }
+
+        System.out.println((System.nanoTime() - start) / 1000000000.0);
+        System.out.println("sum = " + sum);
+    }
+}
