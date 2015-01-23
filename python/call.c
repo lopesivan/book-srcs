a65c22ccf348117fa000e15cd8613ffd6f3ada91
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-01-23 18:06:51 -0200

    Trying to fix

--- python/call.c
+++ python/call.c
@@ -1,36 +1,61 @@
 #include <Python.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <setjmp.h>
+
+#define MODULE_NAME "mymodule"
+#define FUNCTION_NAME "multiply"
 
 int main(int argc, char *argv[])
 {
   PyObject *pModule, *pDict, *pFunc;
   PyObject *pArgs, *pValue;
+  wchar_t *paths;
+  jmp_buf jb;
   int i;
 
-  if (argc < 3) 
+  if (argc != 3)
   {
-    fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
+    fprintf(stderr, "usage: call param1 param2.\n");
     return 1;
   }
 
   Py_Initialize();
-
-  pModule = PyImport_ImportModule(argv[1]);
-  if (pModule != NULL) 
+  if (!Py_IsInitialized())
   {
-    pFunc = PyObject_GetAttrString(pModule, argv[2]);
-    /* pFunc is a new reference */
+    fprintf(stderr, "Python was not initialized!\n");
+    return 1;
+  }
+
+  paths = Py_GetPath();
+  wprintf(L"Path: %s\n", paths);
+
+  /* Finaliza, em caso de erro. */
+  if (setjmp(jb))
+  {
+    Py_Finalize();
+    return 1;
+  }
+
+  if ((pModule = PyImport_ImportModule(MODULE_NAME)) != NULL)
+  {
+    pFunc = PyObject_GetAttrString(pModule, FUNCTION_NAME);
+    Py_DECREF(pModule);
 
     if (pFunc && PyCallable_Check(pFunc)) 
     {
-      pArgs = PyTuple_New(argc - 3);
-      for (i = 0; i < argc - 3; ++i) {
-        pValue = PyInt_FromLong(atoi(argv[i + 3]));
-        if (!pValue) 
+      argv++;
+      argc--;
+
+      pArgs = PyTuple_New(argc);
+      for (i = 0; i < argc; i++) 
+      {
+        if ((pValue = PyInt_FromLong(atoi(*argv++))) != NULL)
         {
           Py_DECREF(pArgs);
-          Py_DECREF(pModule);
+          Py_DECREF(pFunc);
           fprintf(stderr, "Cannot convert argument\n");
-          return 1;
+          longjmp(jb, 1);
         }
 
         /* pValue reference stolen here: */
@@ -39,6 +64,7 @@ int main(int argc, char *argv[])
 
       pValue = PyObject_CallObject(pFunc, pArgs);
       Py_DECREF(pArgs);
+      Py_DECREF(pFunc);
 
       if (pValue != NULL) 
       {
@@ -47,11 +73,9 @@ int main(int argc, char *argv[])
       }
       else 
       {
-        Py_DECREF(pFunc);
-        Py_DECREF(pModule);
         PyErr_Print();
         fprintf(stderr,"Call failed\n");
-        return 1;
+        longjmp(jb, 1);
       }
     }
     else 
@@ -59,18 +83,16 @@ int main(int argc, char *argv[])
       if (PyErr_Occurred())
         PyErr_Print();
 
-      fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+      fprintf(stderr, "Cannot find function \"" FUNCTION_NAME "\"\n");
     }
 
-    Py_XDECREF(pFunc);
     Py_DECREF(pModule);
   }
   else 
   {
     PyErr_Print();
-    fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
-
-    return 1;
+    fprintf(stderr, "Failed to load \"" MODULE_NAME "\"\n");
+    longjmp(jb, 1);
   }
   
   Py_Finalize();
