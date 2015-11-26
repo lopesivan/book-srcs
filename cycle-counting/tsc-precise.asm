9e34a6567a8e62471f6ae70e7e1505321080f97f
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-11-26 17:29:57 -0200

    Better use of rdtsc/rdtscp.

--- cycle-counting/tsc-precise.asm
+++ cycle-counting/tsc-precise.asm
@@ -25,49 +25,56 @@ section .data
 ; Declarações equivalentes:
 ;   static unsigned long count, tmp;
 ;
-        align 16
-count:  dq    0
-tmp:    dq    0
+            align 16
+old_count:  dq    0
+tmp:        dq    0
 
 section .text
 
 global TSC_READ_START:function
 global TSC_READ_END:function
 
-  align 16
+align 16
 ; Protótipo:
 ;   void TSC_READ_START(void);
 ;
 TSC_READ_START:
-  prefetchw [count]   ; Tenta garantir que as duas variáveis temporárias
-                      ; estejam no cache e prontas para serem escritas.
-  mov   [rsp+8],ebx
+  ; Serializa CPU.
+  xor   eax,eax
+  push  rbx
   cpuid
-  mov   ebx,[rsp+8]
+  pop   rbx
+
+  ; Certifica-se que todas as leituras e escritas pendentes
+  ; sejam feitas.
+  mfence
+
   rdtsc
-  mov   [count],eax   ; 1.5 ciclos
-  mov   [count+4],edx ; 1.5 ciclos
-  ret                 ; 8 ciclos.
+  mov   [old_count],eax   ; 1.5 ciclos
+  mov   [old_count+4],edx ; 1.5 ciclos
+
+  ret                     ; 8 ciclos.
 
 ; Protótipo:
 ;   unsigned long TSC_READ_END(void);
 ;
+; OBS: CALL para essa função toma 1 ciclo.
   align 16
 TSC_READ_END:
-                      ; call para essa função toma 1 ciclo.
-  rdtscp
-  mov   [tmp],eax
-  mov   [tmp+4],edx
-  mov   [rsp+8],rbx
-  cpuid
-  mov   rax,[tmp]
-  sub   rax,[count]
-  sub   rax,WASTED_CYCLES  ; subtrai todos os "13.5" ciclos "extras".
+  rdtscp              ; Lê TSC, serializando o processador.
+                      ; Obs: As partes superiores de RAX e RDX estarão zeradas!
+
+  ; Faz RAX <- EDX:EAX.
+  shl   rdx,32
+  or    rax,rdx
+
+  ; RAX = TSC - old_count - WASTED_CYCLES
+  sub   rax,[old_count]
+  sbb   rax,WASTED_CYCLES
 
   ; if (rax <= 0)
   ;   rax = 1;
-  mov    rbx,1
-  cmovle rax,rbx
+  mov    rdx,1
+  cmovle rax,rdx
 
-  mov    rbx,[rsp+8]
   ret
